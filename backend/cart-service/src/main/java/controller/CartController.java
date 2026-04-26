940aa9d60259eaaed428d1e9c888e6a5bf141c5d
Akhilrao3735 <Parfew12212@gmail.com>
2026-04-26 13:22:52 -0400

    1E-controller-crud: CartController APIs created in cart-service

---
+++ backend/cart-service/src/main/java/controller/CartController.java
@@ -0,0 +1,38 @@
+package com.microservices.cartservice.controller;
+
+import com.microservices.cartservice.entity.Cart;
+import com.microservices.cartservice.entity.CartItem;
+import com.microservices.cartservice.service.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/carts")
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping
+    public ResponseEntity<Cart> createCart(@RequestBody Cart cart) {
+        return ResponseEntity.ok(cartService.createCart(cart));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cart> getCartById(@PathVariable Integer id) {
+        return ResponseEntity.ok(cartService.getCartById(id));
+    }
+
+    @PostMapping("/items")
+    public ResponseEntity<CartItem> addItemToCart(@RequestBody CartItem cartItem) {
+        return ResponseEntity.ok(cartService.addItemToCart(cartItem));
+    }
+
+    @GetMapping("/{cartId}/items")
+    public ResponseEntity<List<CartItem>> getCartItems(@PathVariable Integer cartId) {
+        return ResponseEntity.ok(cartService.getCartItems(cartId));
+    }
+}
