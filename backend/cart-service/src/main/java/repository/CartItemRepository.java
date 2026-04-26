d5bc91c017d294c6f2a02b9e536f5539e93271d4
Akhilrao3735 <Parfew12212@gmail.com>
2026-04-26 13:01:54 -0400

    1C-repository-layer: CartRepository and CartItemRepository created in cart-service

---
+++ backend/cart-service/src/main/java/repository/CartItemRepository.java
@@ -0,0 +1,9 @@
+package com.microservices.cartservice.repository;
+
+import com.microservices.cartservice.entity.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
+}
